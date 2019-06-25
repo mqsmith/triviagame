007492b8e04d9c040e6eadae694fe760d4158c81
Mike Smith <smith.mike@mac.com>
2019-06-25 16:01:30 -0400

    Added boiler plate and bootstrap row and columns to the index file. Added reset css and added psudo coding and global vaiables to my app.js.

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+// Questions and answers Array
+
+  
+  // Global variables
+  var game;
+  var counter = 0;
+  var clock;
+  var timer = 30;
+  var correctCounter = 0;
+  var incorrectCounter = 0;
+  var unansweredCounter = 0;
+  
+  $(document).ready(function() {
+    // Start the game when that start button is clicked
+   
+  
+     
+   
+  });
+  // Correct Answer Function
+  
+  // Incorrect Answer Function
+  
+  // Didn't Answer Function
+  
+  
+  // Start the game
+  
+  
+ 
+  
+  // Timer function
+  
+  
+  // Shows results and allows user to restart game
+  
+  
+  // Reset the game
+  
